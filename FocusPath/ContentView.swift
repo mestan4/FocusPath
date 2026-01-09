e385578970cc4af5ab422d12b99e95b751da21eb
Tolgahan Mestan Kaya <mestankaya07@gmail.com>
2026-01-10 01:07:27 +0300

    Initial Commit

---
+++ FocusPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusPath
+//
+//  Created by Tolgahan Mestan on 10.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusPath/FocusPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusPathApp.swift
+//  FocusPath
+//
+//  Created by Tolgahan Mestan on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
