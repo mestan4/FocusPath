30c37815ef18c65f86c3297d18c3a5d95eec13b6
Tolgahan Mestan Kaya <mestankaya07@gmail.com>
2026-01-12 02:33:03 +0300

    ilk swift projesi ve sayac

--- FocusPath/ContentView.swift
+++ FocusPath/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var hedefSayisi = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List{
+                Section(header: Text("Durum")){
+                    Text("Tamamlanan Hedef: \(hedefSayisi)")
+                        .font(.title3)
+                        .bold()
+                }
+                Section(header: Text("Planlarım.")){
+                    Text("SwiftUI Mantığını Kavramak.")
+                    Text("Github Commitlerini Yapmak.")
+                    Text("Viskiyi Tazele.")
+                }
+                
+                Button(action: {
+                    hedefSayisi += 1
+                }) {
+                    HStack {
+                        Image(systemName: "checkmark.circle.fill")
+                        Text("Bir hedef daha tamamlandı!")
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .navigationTitle("Focus Path")
         }
-        .padding()
     }
 }
 
